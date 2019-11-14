0e94d26d9328a348a66eeec478bf71361983824a
Molly <pixieofhugs@gmail.com>
2019-11-14 09:15:53 -0800

    Moved javascript to app.js

---
+++ assignment5/app.js
@@ -0,0 +1,7 @@
+
+
+
+function submit() {
+        console.log(document.getElementById("name").value);
+        console.log(document.getElementById("email").value);
+        console.log(document.getElementById("textarea").value);
